ac95d3bd2b7d405110cfcdfcd0f5b9373b47793f
Negesis23 <86001290+negesis23@users.noreply.github.com>
2023-08-03 14:06:19 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+  const [clicked, setClicked] = useState(0);
   return (
     <div>
       <h1>Hello, World!</h1>
+      <Button onClick={setClicked(clicked + 1)}>Clicked: {clicked}</Button>
     </div>
   );
 };
